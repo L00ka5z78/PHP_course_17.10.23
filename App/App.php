ceec34594d58877f02b11b8dc0d4b2b364e0d465
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-18 10:35:55 +0200

    declare strict types issue. placement is ok but throws error 18.10.23 10:35

--- App/App.php
+++ App/App.php
@@ -1,17 +1,38 @@
-<?php
+ <?php
 
-declare(strict_types=1);
+    // declare(strict_types=1);
 
-function getTransactionFiles()
-// function getTransactionFiles(): array
-{
-    $files = [];
+    function getTransactionFiles(string $dirPath): array
+    // function getTransactionFiles(): array
+    {
+        $files = [];
 
-    foreach (scandir(FILES_PATH) as $file) {
-        if (is_dir($file)) {
-            continue;
+        foreach (scandir($dirPath) as $file) {
+            if (is_dir($file)) {
+                continue;
+            }
+            // getting correct file path
+            $files[] = $dirPath . $file;
         }
-        $files[] = $file;
+        return $files;
     }
-    return $files;
-}
+    // get transactions
+
+    function getTransactions(string $fileName): array
+    {
+        //check if file exist and trigger error if not
+        if (!file_exists($fileName)) {
+            trigger_error('File"' . $fileName . '" does not exist', E_USER_ERROR);
+        }
+        // if exist , open for reading === 'r'
+        $file = fopen($fileName, 'r');
+
+        fgetcsv($file);
+
+        $transactions = [];
+
+        while (($transaction = fgetcsv($file)) !== false) {
+            $transactions[] = $transaction;
+        }
+        return $transactions;
+    }
--- public/index.php
+++ public/index.php
@@ -1,15 +1,26 @@
-<?php
+ <?php
 
-declare(strict_types=1);
+    // declare(strict_types=1);
 
-$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+    $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
-define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+    define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+    define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+    define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
-require APP_PATH . "App.php";
+    require APP_PATH . "App.php";
 
-$files = getTransactionFiles();
+    // getting correct file transaction path
 
-var_dump($files);
+    $files = getTransactionFiles(FILES_PATH);
+
+    /**get transaction. create empty arr and loop over file path and get transaction from path and merging into transactions variable  */
+
+    $transactions = [];
+
+    foreach ($files as $file) {
+        $transactions = array_merge($transactions, getTransactions($file));
+    }
+
+    print_r($files);
+    var_dump($files);
