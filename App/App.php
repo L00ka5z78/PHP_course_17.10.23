7d6602a3c48eeff4f2ef0cef347de789676dc66f
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-18 16:27:10 +0200

    final version 18.10.23 16:27

--- App/App.php
+++ App/App.php
@@ -1,38 +1,72 @@
- <?php
+<?php
 
-    // declare(strict_types=1);
+declare(strict_types=1);
 
-    function getTransactionFiles(string $dirPath): array
-    // function getTransactionFiles(): array
-    {
-        $files = [];
+function getTransactionFiles(string $dirPath): array
+{
+    $files = [];
 
-        foreach (scandir($dirPath) as $file) {
-            if (is_dir($file)) {
-                continue;
-            }
-            // getting correct file path
-            $files[] = $dirPath . $file;
+    foreach (scandir($dirPath) as $file) {
+        if (is_dir($file)) {
+            continue;
         }
-        return $files;
+
+        $files[] = $dirPath . $file;
     }
-    // get transactions
 
-    function getTransactions(string $fileName): array
-    {
-        //check if file exist and trigger error if not
-        if (!file_exists($fileName)) {
-            trigger_error('File"' . $fileName . '" does not exist', E_USER_ERROR);
-        }
-        // if exist , open for reading === 'r'
-        $file = fopen($fileName, 'r');
+    return $files;
+}
 
-        fgetcsv($file);
-
-        $transactions = [];
-
-        while (($transaction = fgetcsv($file)) !== false) {
-            $transactions[] = $transaction;
-        }
-        return $transactions;
+function getTransactions(string $fileName, ?callable $transactionHandler = null): array
+{
+    if (!file_exists($fileName)) {
+        trigger_error('File "' . $fileName . '" does not exist.', E_USER_ERROR);
     }
+
+    $file = fopen($fileName, 'r');
+
+    fgetcsv($file);
+
+    $transactions = [];
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        if ($transactionHandler !== null) {
+            $transaction = $transactionHandler($transaction);
+        }
+
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array
+{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return [
+        'date'        => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount'      => $amount,
+    ];
+}
+
+function calculateTotals(array $transactions): array
+{
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach ($transactions as $transaction) {
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] >= 0) {
+            $totals['totalIncome'] += $transaction['amount'];
+        } else {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+
+    return $totals;
+}
--- public/index.php
+++ public/index.php
@@ -1,26 +1,23 @@
- <?php
+<?php
 
-    // declare(strict_types=1);
+declare(strict_types=1);
 
-    $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
-    define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
-    define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
-    define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
-    require APP_PATH . "App.php";
+require APP_PATH . "App.php";
+require APP_PATH . 'helpers.php';
 
-    // getting correct file transaction path
+$files = getTransactionFiles(FILES_PATH);
 
-    $files = getTransactionFiles(FILES_PATH);
+$transactions = [];
+foreach ($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file, 'extractTransaction'));
+}
 
-    /**get transaction. create empty arr and loop over file path and get transaction from path and merging into transactions variable  */
+$totals = calculateTotals($transactions);
 
-    $transactions = [];
-
-    foreach ($files as $file) {
-        $transactions = array_merge($transactions, getTransactions($file));
-    }
-
-    print_r($files);
-    var_dump($files);
+require VIEWS_PATH . 'transactions.php';
