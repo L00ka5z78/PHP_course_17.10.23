1dd662cf372a0c138ebe75e6ef84db50332d0896
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-19 10:21:59 +0200

    obj and classes 19.10.23 10:21

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 
+require_once '../PaymentProfile.php';
+require_once '../Customer.php';
 require_once '../Transaction.php';
 
 // clssses and objects
@@ -11,9 +13,15 @@ require_once '../Transaction.php';
 // $transaction = new Transaction(100, 'transaction 1');
 // $transaction->addTax(8)->applyDiscount(10);
 
-$amount = (new Transaction(100, 'transaction 1'))
-    ->addTax(8)
-    ->applyDiscount(10)
-    ->getAmount();
+// $amount = (new Transaction(100, 'transaction 1'));
+// ->addTax(8)
+// ->applyDiscount(10)
+// ->getAmount();
 
-var_dump($amount);
+// var_dump($amount);
+
+$transaction = new Transaction(5, "test");
+
+// $transaction->customer = new Customer();
+
+echo $transaction->getCustomer()->getPaymentProfile()->id ?? 'foo';
