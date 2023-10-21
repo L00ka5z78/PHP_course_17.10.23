68626b95ea814964a6a00cfad32c01f3e969f881
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-21 10:07:32 +0200

    exceptions: try catch finally  21.10.23 10:07

--- src/public/index.php
+++ src/public/index.php
@@ -2,10 +2,18 @@
 
 use App\Invoice;
 use App\Customer;
+use Ramsey\Uuid\Exception\InvalidBytesException;
+
 // namespace App; // when i use this, dont need to write App in the path 
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new Invoice(new Customer());
 
-$invoice->process(25);
+try {
+    $invoice->process(25);
+} catch (\App\Exception\MissingBillingException | InvalidBytesException $e) {
+    echo $e->getMessage() . PHP_EOL;
+} finally {
+    echo 'Finally block' . PHP_EOL;
+}
