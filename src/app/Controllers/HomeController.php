fd71554d41a6730c9ba541282b7ab84fbc1345e3
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-11-04 09:37:40 +0100

    di container second way starter 04,11,23 09:37

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -4,35 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\App;
+use App\Services\InvoiceService;
 use App\View;
-use PDO;
-use PDOException;
-use App\Models\Invoice;
-use App\Models\User;
-use App\Models\SignUp;
 
 class HomeController
 {
+    public function __construct(private InvoiceService $invoiceService)
+    {
+    }
+
     public function index(): View
     {
-        $email = 'abc@ex.con';
-        $name = 'jane doe';
-        $amount = 25;
+        $this->invoiceService->process([], 25);
 
-        $userModel    = new User();
-        $invoiceModel = new Invoice();
-
-        $invoiceId = (new SignUp($userModel, $invoiceModel))->register(
-            [
-                'email' => $email,
-                'name' => $name,
-            ],
-            [
-                'amount' => $amount,
-            ]
-        );
-
-        return View::make('index', ['invoice' => $invoiceModel->find($invoiceId)]);
+        return View::make('index');
     }
 }
