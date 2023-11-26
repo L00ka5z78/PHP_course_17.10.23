ac2667a8610fef6b3a586b2fd7333d90d4d69946
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-11-26 18:52:07 +0100

    data mapper 26.11.23 18:52

--- src/app/doctrine_db_example.php
+++ src/app/doctrine_db_example.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\InvoiceStatus;
+use DateTime;
 use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -11,14 +13,41 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-$connectionParams = [
-	'dbname'   => $_ENV['DB_DATABASE'],
-	'user'     =>  $_ENV['DB_USER'],
+$params = [
+	'host' => $_ENV['DB_HOST'],
+	'user' => $_ENV['DB_USER'],
 	'password' => $_ENV['DB_PASS'],
-	'driver'   => $_ENV['DB_DRIVER'] ?? 'pdo_mysql'
+	'dbname' => $_ENV['DB_DATABASE'],
+	'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
 ];
+$entityManager =  EntityManager::create($params, $setup::createAttributeMetadataConfiguration([__DIR__ . '/Entity']));
 
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
 
-$schema = $conn->createSchemaManager();
-var_dump($schema->listTableNames());
+$items = [['Item 1', 1, 15], ['Item 2', 2, 7.5], ['Item 3', 4, 3.75]];
+
+$invoice = (new \App\Entity\Invoice())
+	->setAmount(45)
+	->setInvoiceNumber('1')
+	->setStatus(InvoiceStatus::Pending)
+	->setCreatedAt(new DateTime());
+
+foreach ($items as [$description, $quantity, $unitPrice]) {
+	$item = (new \App\Entity\InvoiceItem())
+		->setDescription($description)
+		->setQuantity($quantity)
+		->setUnitPrice($unitPrice);
+
+	$invoice->addItem($item);
+	$entityManager->persist($item);
+}
+$entityManager->persist($invoice);
+$entityManager->flush();
+
+$entityManager->remove($invoice);
+
+$invoice->getItems()->get(0)->setDescription('FOOBar');
+
+
+$entityManager->flush();
+
+echo $entityManager->getUnitOfWork->size();
